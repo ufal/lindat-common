4e21f75c6be0e37bfde21c815d964e469af7e514
Michal Sedlak <sedlakmichal@gmail.com>
2015-08-04 14:30:24 +0200

    Test cleanup

--- test/basic.js
+++ test/basic.js
@@ -1,36 +1,11 @@
 describe('Lindat Common', function() {
   var header = element(by.css('.lindat-header'));
   var footer = element(by.css('.lindat-footer'));
-  var body = element(by.tagName('body'));
   var EC = protractor.ExpectedConditions;
   var waitingTime = 5000;
-  var sleepTime = 3000;
 
   beforeEach(function() {
-    jasmine.addMatchers({
-      toBeTransparent: function (util, customEqualityTesters) {
-        return {
-          compare: function (actual) {
-            var result = {};
-            result.pass = util.equals(actual, 'transparent', customEqualityTesters) ||
-              util.equals(actual, 'rgba(0, 0, 0, 0)', customEqualityTesters);
-            if (result.pass) {
-              result.message = 'Expected ' + actual + ' not to be transparent';
-            } else {
-              result.message = 'Expected ' + actual + ' to be transparent';
-            }
-
-            return result;
-          }
-        };
-      }
-    });
-
     browser.get('index.html');
-  });
-
-  afterEach(function() {
-    browser.executeScript('window.sessionStorage.clear(); window.localStorage.clear();');
   });
 
   it('should load', function() {
