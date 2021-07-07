adbad3cdbfd99e3f5884583809d7793d2b823b72
Ondřej Košarko <ko_ok@centrum.cz>
2021-07-07 15:51:12 +0200

    test that check there is the same number of items as in drupal

--- test/new_theme.js
+++ test/new_theme.js
@@ -1,12 +1,13 @@
+var headerLocator = by.js(function(){
+  return document.querySelector('lindat-header').shadowRoot.querySelector('header');
+})
+var footerLocator = by.js(function(){
+  return document.querySelector('lindat-footer').shadowRoot.querySelector('footer');
+})
+var EC = protractor.ExpectedConditions;
+var waitingTime = 5000;
+
 describe('Lindat Common new_theme', function() {
-  var headerLocator = by.js(function(){
-    return document.querySelector('lindat-header').shadowRoot.querySelector('header');
-  })
-  var footerLocator = by.js(function(){
-    return document.querySelector('lindat-footer').shadowRoot.querySelector('footer');
-  })
-  var EC = protractor.ExpectedConditions;
-  var waitingTime = 5000;
 
   async function attributePresent(elementLocator, attrName){
     let e = await element(elementLocator)
@@ -16,23 +17,14 @@ describe('Lindat Common new_theme', function() {
   }
 
   beforeEach(async function() {
-    browser.get('/new_theme/index.html');
+    await browser.get('/dist/new_theme/example/index.html');
     await browser.wait(EC.presenceOf(element(headerLocator)), waitingTime)
     await browser.wait(EC.presenceOf(element(footerLocator)), waitingTime)
   });
 
-  /*    it('should load', function() {
-        expect(browser.getTitle()).toEqual('Test page title');
-      });
-
-      it('should have Lindat header', async function () {
-        let header = await browser.driver.findElement(headerLocator)
-
-        var menu = header.findElement(By.css('.lindat-menu'));
-        expect(menu.getCssValue('height')).toBe('53px');
-        var menuItems = header.findElements(By.css('.lindat-menu li'));
-        expect(menuItems.count()).toEqual(8);
-      });*/
+  it('should load', async function() {
+    expect(await browser.getTitle()).toEqual('Test page title');
+  });
 
   it('footer should have data-version', async function (){
     await attributePresent(footerLocator, "data-version")
@@ -41,6 +33,35 @@ describe('Lindat Common new_theme', function() {
   it('footer should have data-build', async function (){
     await attributePresent(footerLocator, "data-build")
   });
-  /*
-  */
+  it('header should have data-version', async function (){
+    await attributePresent(headerLocator, "data-version")
+  });
+
+  it('header should have data-build', async function (){
+    await attributePresent(headerLocator, "data-build")
+  });
+});
+
+describe('lindat-common matches drupal', function (){
+  it('common header should have the same number of items as lindat.cz', async function(){
+    //drupal header
+    await browser.get('https://lindat.cz');
+    let menu = await element(by.css(".block--clariah-theme-main-menu"))
+    await browser.wait(EC.presenceOf(menu), waitingTime)
+    let expected_nav_items = await menu.all(by.css(".nav-item")).count()
+    let expected_dropdown_items = await menu.all(by.css(".nav-item .dropdown-item")).count()
+
+    expect(expected_nav_items).toBeDefined()
+
+    //common header
+    await browser.get('/dist/new_theme/example/index.html');
+    let header = await element(headerLocator)
+    await browser.wait(EC.presenceOf(header), waitingTime)
+    let real_nav_items = await header.all(by.css(".block--clariah-theme-main-menu .nav-item")).count()
+    let real_dropdown_items = await header.all(by.css(".block--clariah-theme-main-menu .nav-item .dropdown-item")).count()
+    expect(real_nav_items).toBe(expected_nav_items);
+    //console.log(`========= ${real_nav_items}=${expected_nav_items}`)
+    expect(real_dropdown_items).toBe(expected_dropdown_items);
+  })
+
 });
