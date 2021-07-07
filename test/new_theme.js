b291ad22a705ac68a77eeeee202274efb78168b6
Ondřej Košarko <ko_ok@centrum.cz>
2021-07-07 16:53:54 +0200

    update new theme test, trying to load lindat.cz only once

--- test/new_theme.js
+++ test/new_theme.js
@@ -43,18 +43,28 @@ describe('Lindat Common new_theme', function() {
 });
 
 describe('lindat-common matches drupal', function (){
-  it('common header should have the same number of items as lindat.cz', async function(){
+
+  let epected_nav_items, expected_dropdown_items, expected_footer_anchors;
+
+  beforeAll(async function(){
     //drupal header
     await browser.get('https://lindat.cz');
     let menu = await element(by.css(".block--clariah-theme-main-menu"))
     await browser.wait(EC.presenceOf(menu), waitingTime)
-    let expected_nav_items = await menu.all(by.css(".nav-item")).count()
-    let expected_dropdown_items = await menu.all(by.css(".nav-item .dropdown-item")).count()
+    expected_nav_items = await menu.all(by.css(".nav-item")).count()
+    expected_dropdown_items = await menu.all(by.css(".nav-item .dropdown-item")).count()
 
+    //drupal footer
+    let footer = await element(by.css("footer"))
+    await browser.wait(EC.presenceOf(footer), waitingTime)
+    expected_footer_anchors = await footer.all(by.css("a")).count()
+
+    await browser.get('/dist/new_theme/example/index.html');
+  })
+
+  it('common header should have the same number of items as lindat.cz', async function(){
     expect(expected_nav_items).toBeDefined()
 
-    //common header
-    await browser.get('/dist/new_theme/example/index.html');
     let header = await element(headerLocator)
     await browser.wait(EC.presenceOf(header), waitingTime)
     let real_nav_items = await header.all(by.css(".block--clariah-theme-main-menu .nav-item")).count()
@@ -65,14 +75,8 @@ describe('lindat-common matches drupal', function (){
   });
 
   it('footer should have the same number of links as lindat.cz', async function(){
-    //drupal footer
-    await browser.get('https://lindat.cz');
-    let footer = await element(by.css("footer"))
-    await browser.wait(EC.presenceOf(footer), waitingTime)
-    let expected_footer_anchors = await footer.all(by.css("a")).count()
     expect(expected_footer_anchors).toBeDefined()
 
-    await browser.get('/dist/new_theme/example/index.html');
     let common_footer = await element(footerLocator)
     let real_footer_anchors = await common_footer.all(by.css("a")).count()
 
